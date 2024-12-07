a26ec4f1b9e102f9ef62db1567d23a7907e3cb70
EzekielAd <114159405+EzekielAd@users.noreply.github.com>
2024-12-07 09:10:18 -0500

    Complete

--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,26 @@ router.get('/project', function(req, res, next) {
   res.render('Projects', { title: 'Projects Page' });
 });
 
+/* GET Project1 description page. */
+router.get('/project1', function(req, res, next) {
+  res.render('Projects1', { title: 'Project 1 Page' });
+});
+
+/* GET Project2 description page. */
+router.get('/project2', function(req, res, next) {
+  res.render('Project2', { title: 'Project 2 Page' });
+});
+
+/* GET Project3 description page. */
+router.get('/project3', function(req, res, next) {
+  res.render('Project3', { title: 'Project 3 Page' });
+});
+
+/* GET Project4 description page. */
+router.get('/project4', function(req, res, next) {
+  res.render('Project4', { title: 'Project 4 Page' });
+});
+
 /* GET Contact me page. */
 router.get('/contact', function(req, res, next) {
   res.render('Contact', { title: 'Contact me' });
